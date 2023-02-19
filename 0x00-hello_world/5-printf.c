3dc8cac3a758ea9fbb4608f2f7fd5814187082c2
Sindisiwe95 <sindisiwembambo01@gmail.com>
2023-02-18 20:01:27 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints "with proper grammar, but the outcome is a piece of art," followed newline
- * * Return: Always 0 (Success)
+ ** Return: Always 0 (Success)
  */
 
 int main(void)
